d9e0321a0501caae8056467f9f4361c738a6b3af
eduardocastro465 <hiasd12e@gmail.com>
2026-09-10 10:40:04 -0600

    fix(auth): corrección en rutas de autenticación y registro de usuarios

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,14 @@
 // routes/usuarios.routes.ts
 import { Router } from 'express';
-import { login } from '../controllers/auth.controller.js';
+import { Login, logout, logoutAll, refreshToken, registrarUsuario } from '../controllers/auth.controller.js';
 
 const router = Router();
 
-router.post('/login', login);
+router.post('/login', Login);
+router.post('/register', registrarUsuario);
+router.post('/logout', logout);
+router.post('/logoutAll', logoutAll);
+router.post('/refreshToken', refreshToken);
 
 
 export default router;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createUser, deleteUser, getUserById, getUserEmail,updateUser ,getUserList, updateFotoPerfil , updateEmail, updatePhone } from "../controllers/user.controller.js";
+import {  deleteUser, getUserById, getUserEmail,updateUser ,getUserList, updateFotoPerfil , updateEmail, updatePhone } from "../controllers/user.controller.js";
 import { upload } from "../middleware/upload.middleware.js";
 
 const router = Router();
@@ -7,7 +7,6 @@ const router = Router();
 router.get('/', getUserList);
 router.get('/email/:correo', getUserEmail);
 router.get('/:id', getUserById);
-router.post('/', upload.single('fotoPerfil'), createUser);
 router.put('/:id', updateUser);
 router.put('/:id/img', upload.single('fotoPerfil'), updateFotoPerfil);
 router.put('/:id/email', updateEmail);
